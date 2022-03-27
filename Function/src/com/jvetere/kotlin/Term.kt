02753dbc3d17acf643ac1f72ecd275c48625dbb3
jacobvetere <jvetere1999@gmail.com>
2022-03-27 15:23:07 -0400

    Turned Term into linked list nodes Added flags for algebraic and tragicomic functions TODO: Fix calculation with this Completely overhauled regular expression system for breaking apart terms (Still testing) Began work on Function.kt TODO: Create grid object for graphing equations in a representative form TODO: Restrict this grid to uneven square shape and override get operators to accept -(len/2-1) to (len/2) instead of (0 to len-1) TODO: Add sin and inverse sin grid scanning for grid updates
    Signed-off-by: jacobvetere <jvetere1999@gmail.com>

--- Function/src/com/jvetere/kotlin/Term.kt
+++ Function/src/com/jvetere/kotlin/Term.kt
@@ -1,24 +1,82 @@
 package com.jvetere.kotlin
 
 import org.junit.jupiter.api.assertThrows
+import java.lang.StrictMath.pow
 
-data class Term(val ne  : Boolean, val const : Int, val variable : Char, val exponent : Int) : Exception("ERR: Invalid input!");
-
-fun createTerm(exp : String) : Term{
-    val regex: Regex = Regex("((?:[+-])|(?:\\d+)|(?:x))");
-    val token: List<Any> = regex.findAll(exp).toList();
-    if (token[0] == "-")
-        TODO( "Figure out how to remove the element from the beging of array if is a sign and create the neg value from that")
-    val term : Term = when (token.size) {
-        3 -> { Term(token[0] == "-", token[1] as Int, token[2] as Char, token[3] as Int) }
-        2 -> { TODO( "Implement 2 char long parsing for term")}
-        1 -> { TODO("Implement 1 char long parsing for term")}
-
-        else -> { assertThrows<Term> {}}
+data class Term(val neg  : Boolean, val const : Int, var variable : String, val exponent : Int, var next: Term?, var flag: OuterFunctionFlag) : TypeCastException("ERR: Invalid input!") {
+    var negEx: Boolean = false;
+    init {
+        negEx = variable.contains('-')
+        if (negEx) variable = variable.substring(1);
     }
-    return Term(true,1, 'x',1);
+    fun calculate(x: Int) : Int {
+        val value: Double = const * (pow(x.toDouble(), exponent.toDouble()));
+        return (if (neg) -value else value).toInt()
+        //TODO("Add calculate by flag")
+    }
+    fun derive() : Term? {
+        if(exponent == 0)
+            return null;
+        //TODO("Add functionality for deriving functions by flag");
+        return if (next != null)
+            Term(neg, const * exponent, variable, exponent-1, next?.derive(), flag)
+        else
+            Term(neg, const * exponent, variable, exponent-1, next?.derive(), flag)
+
+    }
+    fun varNeg(): Boolean {
+        return negEx;
+    }
 }
 
-fun isNumeric( s : String?): Boolean {
-    return !s.isNullOrEmpty() && s.matches(Regex("\\d+"));
+
+fun createTerm(exp : String) : Term{
+    val regex: Regex = Regex("((?:sin)|(?:cos)|(?:tan)|(?:ln)|(?:log)|(?:^[+-])|(?:[-]\\d)|(?:\\d+)|(?:x)|(?:[-][x]))")
+    val matches: Sequence<MatchResult> = regex.findAll(exp)
+    val token: MutableList<String> = matches.map{ it.groupValues[1] }.toMutableList()
+    var ne: Boolean = false
+    var flag: OuterFunctionFlag = OuterFunctionFlag.NA
+    var constMultiplyer: Int = 1;
+    //TODO("split these off into sub functioins"):
+    if (token.contains("-") || token.contains("+")) {
+        ne = token[0] == "-"
+        token.removeAt(0)
+    }
+    if (hasOp(token)) {
+        flag = assignFlag(token[0])
+        token.removeAt(0)
+    }
+    if (isNumeric(token[0]) && token.size > 2){
+        constMultiplyer = Integer.parseInt(token[0])
+        token.removeAt(0);
+    }
+    val term : Term = when (token.size) {
+        3 -> { Term(ne, (Integer.parseInt(token[0])*constMultiplyer) , token[1], Integer.parseInt(token[2]), null, flag) }
+        2 -> {
+            if (isNumeric(token[0]))
+                Term(ne, (Integer.parseInt(token[0])*constMultiplyer), token[1], 1, null, flag)
+            else
+                Term(ne, (1*constMultiplyer), token[0], Integer.parseInt(token[1]), null, flag)
+        }
+        1 -> {
+            if (isNumeric(token[0]))
+                Term(ne, (Integer.parseInt(token[0])*constMultiplyer), "x", 0, null, flag)
+            else
+                Term(ne, (1*constMultiplyer), token[0], 1, null, flag)
+        }
+        else -> assertThrows<Term> {}
+    }
+    return term
+}
+
+fun isNumeric(s : String): Boolean {
+    return !s.isNullOrEmpty() && s.matches(Regex("\\d+"))
+}
+fun hasOp(l : MutableList<String>): Boolean {
+    if (l.isNullOrEmpty()) return false
+    for (s in l) {
+        if (s.matches(Regex("sin|cos|tan|ln|log")))
+            return true;
+    }
+    return false
 }
--- Function/src/test/com/jvetere/kotlin/TermKtTest.kt
+++ Function/src/test/com/jvetere/kotlin/TermKtTest.kt
@@ -1,5 +1,6 @@
 package test.com.jvetere.kotlin
 
+import com.jvetere.kotlin.OuterFunctionFlag
 import com.jvetere.kotlin.Term
 import com.jvetere.kotlin.createTerm
 import org.junit.jupiter.api.Test
@@ -10,10 +11,158 @@ import kotlin.test.assertEquals
 internal class TermKtTest {
 
     @Test
-    internal fun BasicTerm() {
+    internal fun varIntAndEx() {
         val term: Term = createTerm("2x^2");
         assertAll("Basic",
-            { assertEquals(false, term.ne) },
+            { assertEquals(false,   term.neg) },
+            { assertEquals(2,       term.const) },
+            { assertEquals(2,       term.exponent) },
+            { assertEquals("x",     term.variable) },
+            { assertEquals(2,       term.calculate(1)) }
+        )
+        val firstDerivation: Term? = term.derive()
+        if (firstDerivation != null) {
+            assertAll("Derive",
+                { assertEquals(false,   firstDerivation.neg) },
+                { assertEquals(4,       firstDerivation.const) },
+                { assertEquals(1,       firstDerivation.exponent) },
+                { assertEquals("x",     firstDerivation.variable) },
+                { assertEquals(4,       firstDerivation.calculate(1)) }
             )
+        }
+    }
+    @Test
+    internal fun varIntAndExNeg() {
+        val term: Term = createTerm("-2x^2");
+        assertAll("Basic",
+            { assertEquals(true,    term.neg) },
+            { assertEquals(2,       term.const) },
+            { assertEquals(2,       term.exponent) },
+            { assertEquals("x",     term.variable) },
+            { assertEquals(-8,      term.calculate(2)) }
+        )
+        val firstDerivation: Term? = term.derive()
+        if (firstDerivation != null) {
+            assertAll("Derive",
+                { assertEquals(true,   firstDerivation.neg) },
+                { assertEquals(4,       firstDerivation.const) },
+                { assertEquals(1,       firstDerivation.exponent) },
+                { assertEquals("x",     firstDerivation.variable) },
+                { assertEquals(-4,       firstDerivation.calculate(1)) }
+            )
+        }
+    }
+    @Test
+    internal fun varAndInt() {
+        val term: Term = createTerm("+2x");
+        assertAll("Basic",
+            { assertEquals(false,   term.neg) },
+            { assertEquals(2,       term.const) },
+            { assertEquals(1,       term.exponent) },
+            { assertEquals("x",     term.variable) },
+            { assertEquals(8,       term.calculate(4)) }
+        )
+        val firstDerivation: Term? = term.derive()
+        if (firstDerivation != null) {
+            assertAll("Derive",
+                { assertEquals(false,   firstDerivation.neg) },
+                { assertEquals(2,       firstDerivation.const) },
+                { assertEquals(0,       firstDerivation.exponent) },
+                { assertEquals("x",     firstDerivation.variable) },
+                { assertEquals(2,       firstDerivation.calculate(1)) }
+            )
+        }
+    }
+    @Test
+    internal fun singleIntTerm() {
+        val term: Term = createTerm("2");
+        assertAll("Single Integer",
+            { assertEquals(false,   term.neg) },
+            { assertEquals(2,       term.const) },
+            { assertEquals(0,       term.exponent) },
+            { assertEquals("x",     term.variable) },
+            { assertEquals(2,       term.calculate(4)) },
+            { assertEquals(OuterFunctionFlag.NA, term.flag)}
+        )
+        val firstDerivation: Term? = term.derive()
+        assertAll("Derive",
+            { assertEquals(null,   firstDerivation) }
+        )
+    }
+    @Test
+    internal fun lnTerm() {
+        val term: Term = createTerm("ln2x");
+        assertAll("LN",
+            { assertEquals(false,   term.neg) },
+            { assertEquals(2,       term.const) },
+            { assertEquals(1,       term.exponent) },
+            { assertEquals("x",     term.variable) },
+            { assertEquals(8,       term.calculate(4)) },
+            { assertEquals(OuterFunctionFlag.LN, term.flag)}
+        )
+    }
+    @Test
+    internal fun lnTermFringeNeg() {
+        val term: Term = createTerm("-ln2x^2");
+        assertAll("neg LN",
+            { assertEquals(true,   term.neg) },
+            { assertEquals(2,       term.const) },
+            { assertEquals(2,       term.exponent) },
+            { assertEquals("x",     term.variable) },
+            { assertEquals(8,       term.calculate(4)) },
+            { assertEquals(OuterFunctionFlag.LN, term.flag)}
+        )
+    }
+    @Test
+    internal fun lnTermFringeNegEx() {
+        val term: Term = createTerm("-ln2-x^2");
+        assertAll("neg EX LN",
+            { assertEquals(true,   term.neg) },
+            { assertEquals(2,       term.const) },
+            { assertEquals(2,       term.exponent) },
+            { assertEquals("x",     term.variable) },
+            { assertEquals(8,       term.calculate(4)) },
+            { assertEquals(OuterFunctionFlag.LN, term.flag)},
+            { assertEquals(true, term.negEx)}
+        )
+    }
+    @Test
+    internal fun lnTermFringeNegExponet() {
+        val term: Term = createTerm("-ln2x^-2");
+        assertAll("neg EX LN",
+            { assertEquals(true,   term.neg) },
+            { assertEquals(2,       term.const) },
+            { assertEquals(-2,       term.exponent) },
+            { assertEquals("x",     term.variable) },
+            { assertEquals(8,       term.calculate(4)) },
+            { assertEquals(OuterFunctionFlag.LN, term.flag)},
+            { assertEquals(false, term.negEx)}
+        )
+    }
+    @Test
+    internal fun lnTermFringeNegConst() {
+        val term: Term = createTerm("ln-2x^2");
+        assertAll("neg EX LN",
+            { assertEquals(false,   term.neg) },
+            { assertEquals(-2,       term.const) },
+            { assertEquals(2,       term.exponent) },
+            { assertEquals("x",     term.variable) },
+            { assertEquals(8,       term.calculate(4)) },
+            { assertEquals(OuterFunctionFlag.LN, term.flag)},
+            { assertEquals(false, term.negEx)}
+        )
+    }
+    @Test
+    internal fun lnTermFringeNegExNegStart() { //TODO(broken for other reasons)
+        val term: Term = createTerm("-2ln2x^-2");
+        assertAll("neg EX LN",
+            { assertEquals(true,   term.neg) },
+            { assertEquals(4,       term.const) },
+            { assertEquals(-2,       term.exponent) },
+            { assertEquals("x",     term.variable) },
+            { assertEquals(8,       term.calculate(4)) },
+            { assertEquals(OuterFunctionFlag.LN, term.flag)},
+            { assertEquals(false, term.negEx)}
+        )
     }
 }
