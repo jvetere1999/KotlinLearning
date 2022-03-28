1a7b50ffa33d5b223ca2ba289e6c05f9b7a2090f
jacobvetere <jvetere1999@gmail.com>
2022-03-28 17:36:29 -0400

    Tested things that cannot be tested on my lap top because it keeps whining for its bink

--- Function/src/test/com/jvetere/kotlin/ConceptTest.kt
+++ Function/src/test/com/jvetere/kotlin/ConceptTest.kt
@@ -5,13 +5,36 @@ import org.junit.jupiter.api.assertAll
 
 internal class ConceptTest {
 
-    @Test
+   @Test
     internal fun regex() {
-        val exp: String = "+2x^2"
-        val regex: Regex = Regex("\\(([^\\)]+)\\)|((?:sin)|(?:cos)|(?:tan)|(?:ln)|(?:log)|(?:^[+-])|(?:[-]\\d)|(?:\\d+)|(?:x)|(?:[-][x]))")
-        val matches: Sequence<MatchResult> = regex.findAll(exp)
-        val token: MutableList<String> = matches.map{ it.groupValues[0] }.toMutableList()
-        println(token.toString())
+        val exp: String = "+2x^2";
+        val regex: Regex = Regex("\\(([^\\)]+)\\)|((?:sin)|(?:cos)|(?:tan)|(?:ln)|(?:log)|(?:^[+-])|(?:[-]\\d)|(?:\\d+)|(?:x)|(?:[-][x]))");
+        val matches: Sequence<MatchResult> = regex.findAll(exp);
+        val token: MutableList<String> = matches.map{ it.groupValues[0] }.toMutableList();
+        var neg: Boolean?;
+        var const: Int?;
+        var flag: OuterFunctionFlag?;
+        var variable: String?;
+        var expopnent: Int?;
+        for (x in token){
+            if (isSign(x))
+                neg = token[0] == "-"
+            if (x.matches(Regex("[0-9]+"))){
+                if(flag == null && variable == null && const == null)
+                    const = Integer.pasrseInt(x);
+                else
+                    exponent = Interger.parseInt(x);
+            }
+            if (isOp(x))
+                flag = assignOp(x)
+        }
+        println("$neg $const $flag $variable $exponent")
         assertAll("Regex Tests")
     }
-}//((?:sin)|(?:cos)|(?:tan)|(?:ln)|(?:log)|(?:^[+-])|(?:[-]\d)|(?:\d+)|(?:x)|(?:[-][x]))
+    fun isSign(s : String): Boolean {
+        return !s.isNullOrEmpty() && s.matches(Regex("\\d+|[+-]\\d+"));
+    }
+    fun isOp(s : String): Boolean {
+        return !s.isNullOrEmpty() && s.matches(Regex("sin|cos|tan|ln|log"));
+    }
+}
